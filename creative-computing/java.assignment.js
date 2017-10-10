0c4c3ccd8d19f6e62d41b0deec75839d2edd21c5
swjuri <31457339+swjuri@users.noreply.github.com>
2017-10-10 18:51:35 -0400

    yu

---
+++ creative-computing/java.assignment.js
@@ -0,0 +1,224 @@
+document.write('1');
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (i<5){
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+
+        }
+
+        
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+document.write('2');
+        
+
+    for (var i = 0; i < 10; i = i + 1) {
+        for (var j = 0; j < 10; j = j + 1) {
+            console.log("Row " + i + ", Column " + j);
+
+            // The next line is the only JS you'll have to change.
+            if (j<5){
+                console.log("   pink");
+                document.write("<div class='box pink'></div>");
+            } else {
+                console.log("   turquoise");
+                document.write("<div class='box turquoise'></div>");
+
+            }
+
+            
+        }
+
+        document.write('<br />');
+    }
+
+    document.write('<br />');
+    document.write('3');
+        
+
+        for (var i = 0; i < 10; i = i + 1) {
+            for (var j = 0; j < 10; j = j + 1) {
+                console.log("Row " + i + ", Column " + j);
+
+                // The next line is the only JS you'll have to change.
+                if (j==i){
+                    console.log("   pink");
+                    document.write("<div class='box pink'></div>");
+                } else {
+                    console.log("   turquoise");
+                    document.write("<div class='box turquoise'></div>");
+
+                }
+
+                
+            }
+
+            document.write('<br />');
+        }
+
+        document.write('<br />');
+        document.write('4');
+                
+
+            for (var i = 0; i < 10; i = i + 1) {
+                for (var j = 0; j < 10; j = j + 1) {
+                    console.log("Row " + i + ", Column " + j);
+
+                    // The next line is the only JS you'll have to change.
+                    if (j<i){
+                        console.log("   pink");
+                        document.write("<div class='box pink'></div>");
+                    } else {
+                        console.log("   turquoise");
+                        document.write("<div class='box turquoise'></div>");
+
+                    }
+
+                    
+                }
+
+                document.write('<br />');
+            }
+
+                document.write('<br />');
+            document.write('5');
+                for (var i = 0; i < 10; i = i + 1) {
+                    for (var j = 0; j < 10; j = j + 1) {
+                        console.log("Row " + i + ", Column " + j);
+
+                        // The next line is the only JS you'll have to change.
+                        if ((i/j)&(j/i)&(j**i)){
+                            console.log("   pink");
+                            document.write("<div class='box pink'></div>");
+                        } else {
+                            console.log("   turquoise");
+                            document.write("<div class='box turquoise'></div>");
+
+                        }
+
+                        
+                    }
+
+                    document.write('<br />');
+                }
+
+                document.write('<br />');
+
+                    document.write('<br />');
+                    document.write('6');
+                        for (var i = 0; i < 10; i = i + 1) {
+                            for (var j = 0; j < 10; j = j + 1) {
+                                console.log("Row " + i + ", Column " + j);
+
+                                // The next line is the only JS you'll have to change.
+                                if ((i+j) & (j<10)){
+                                    console.log("   pink");
+                                    document.write("<div class='box pink'></div>");
+                                } else {
+                                    console.log("   turquoise");
+                                    document.write("<div class='box turquoise'></div>");
+
+                                }
+
+                        
+                    }
+
+                    document.write('<br />');
+                }
+
+                document.write('<br />');
+                    document.write('<br />');
+                    document.write('7');
+                        for (var i = 0; i < 10; i = i + 1) {
+                            for (var j = 0; j < 10; j = j + 1) {
+                                console.log("Row " + i + ", Column " + j);
+
+                                // The next line is the only JS you'll have to change.
+                                if ((i**j) & (j<10)){
+                                    console.log("   pink");
+                                    document.write("<div class='box pink'></div>");
+                                } else {
+                                    console.log("   turquoise");
+                                    document.write("<div class='box turquoise'></div>");
+
+                                }
+
+                        
+                    }
+
+                    document.write('<br />');
+                }
+
+                document.write('<br />');
+
+
+
+
+
+
+
+                document.write('<br />');
+                    document.write('<br />');
+                    document.write('7');
+                        for (var i = 0; i < 10; i = i + 1) {
+                            for (var j = 0; j < 10; j = j + 1) {
+                                console.log("Row " + i + ", Column " + j);
+
+                                // The next line is the only JS you'll have to change.
+                                if (3*i+j) {
+                                    console.log("   pink");
+                                    document.write("<div class='box pink'></div>");
+                                } else {
+                                    console.log("   turquoise");
+                                    document.write("<div class='box turquoise'></div>");
+
+                                }
+
+                        
+                    }
+
+                    document.write('<br />');
+                }
+
+                document.write('<br />');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        
---
+++ creative-computing/javascript.js
@@ -0,0 +1,18 @@
+var animals =[
+	"cat",
+	"dog",
+	"pig",
+	"toucan",
+	"turkey",
+	"fish"
+]
+
+for(var i=0; i<animals.length; i=i+1) 
+	{
+	console.log(i)
+	console.log(animals[i]);
+
+}
+
+console.log ("done");
+
