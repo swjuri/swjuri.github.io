3985c0761c610020378a5e53e7b2077911f8c17b
swjuri <31457339+swjuri@users.noreply.github.com>
2017-10-10 18:55:54 -0400

    f

--- creative-computing/java.assignment.js
+++ creative-computing/java.assignment.js
@@ -137,7 +137,32 @@ document.write('2');
                     document.write('<br />');
                 }
 
-                
+                document.write('<br />');
+                    document.write('<br />');
+                    document.write('7');
+                        for (var i = 0; i < 10; i = i + 1) {
+                            for (var j = 0; j < 10; j = j + 1) {
+                                console.log("Row " + i + ", Column " + j);
+
+                                // The next line is the only JS you'll have to change.
+                                if ((i**j) & (j<10)){
+                                    console.log("   pink");
+                                    document.write("<div class='box pink'></div>");
+                                } else {
+                                    console.log("   turquoise");
+                                    document.write("<div class='box turquoise'></div>");
+
+                                }
+
+                        
+                    }
+
+                    document.write('<br />');
+                }
+
+                document.write('<br />');
+
+
 
 
 
