e9aabc409361d6fc5886df74bfa6c304ea958f4d
swjuri <31457339+swjuri@users.noreply.github.com>
2018-05-03 21:33:17 -0400

    h

--- interactive-lab/creative-interaction/core-interaction/sitespecificinterface/flower2.js
+++ interactive-lab/creative-interaction/core-interaction/sitespecificinterface/flower2.js
@@ -1,28 +1,28 @@
 document.getElementById("direction").onclick = function() {
-    document.getElementById("time").style.visibility = "visible";
-    document.getElementById("direction").style.visibility = "hidden";
+    document.getElementById("time").style.display = "inline-block";
+    document.getElementById("direction").style.display = "none";
 }
 
 document.getElementById("quick").onclick = function() {
 
-    document.getElementById("numberquick").style.visibility = "visible";
-    document.getElementById("time").style.visibility = "hidden";
+    document.getElementById("numberquick").style.display= "inline-block";
+    document.getElementById("time").style.display = "none";
 
 
 }
 
 document.getElementById("tries").onclick = function() {
 
-    document.getElementById("numbertries").style.visibility = "visible";
-    document.getElementById("time").style.visibility = "hidden";
+    document.getElementById("numbertries").style.display = "inline-block";
+    document.getElementById("time").style.display = "none";
 
 
 }
 
 document.getElementById("every").onclick = function() {
 
-    document.getElementById("numberevery").style.visibility = "visible";
-    document.getElementById("time").style.visibility = "hidden";
+    document.getElementById("numberevery").style.display = "inline-block";
+    document.getElementById("time").style.display = "none";
 
 
 }
