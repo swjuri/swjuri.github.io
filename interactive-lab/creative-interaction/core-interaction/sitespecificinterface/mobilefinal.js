ae21d36ed61303bac7b6561d40030e8ed3bde383
swjuri <31457339+swjuri@users.noreply.github.com>
2018-05-07 13:08:04 -0400

    vv
    vv

--- interactive-lab/creative-interaction/core-interaction/sitespecificinterface/mobilefinal.js
+++ interactive-lab/creative-interaction/core-interaction/sitespecificinterface/mobilefinal.js
@@ -1,3 +1,83 @@
-$(".card").click(function() {
-  $(".card").toggleClass("swipeLeft");
-});
+(function() {
+  var mousedown = false,
+    card = false,
+    initPos = {},
+    delta = {},
+    rotate = 0;
+
+  document.body.addEventListener("mousedown", touch);
+  document.body.addEventListener("mouseup", release);
+  window.addEventListener("mousemove", move);
+
+  function move(e) {
+    if (mousedown) {
+      var currPos = {};
+      currPos.y = e.clientY || e.touches[0].clientY;
+      currPos.x = e.clientX || e.touches[0].clientX;
+
+      if (!initPos.x) {
+        initPos.y = currPos.y;
+        initPos.x = currPos.x;
+      }
+
+      delta.x = currPos.x - initPos.x;
+      delta.y = currPos.y - initPos.y;
+
+      rotate = delta.x * 0.05;
+
+      var css =
+        "translate(" +
+        delta.x +
+        "px, " +
+        delta.y +
+        "px) rotate(" +
+        rotate +
+        "deg)";
+      card.style.transform = css;
+    }
+  }
+
+  function release() {
+    if (card) {
+      if (delta.x > 50 || delta.x < -50) {
+        var animleft;
+
+        if (delta.x > 50) {
+          animleft = window.innerWidth;
+        } else if (delta.x < -50) {
+          animleft = -window.innerWidth;
+        }
+
+        card.style.transform =
+          "translate(" +
+          animleft +
+          "px, " +
+          delta.y * 4 +
+          "px) rotate(" +
+          rotate * 4 +
+          "deg)";
+
+        setTimeout(function() {
+          var c = document.body.querySelector(".card.animating-out");
+          c.style.transform = "none";
+        }, 500);
+      } else {
+        card.style.transform = "none";
+      }
+    }
+
+    mousedown = false;
+    initPos = {};
+    card = false;
+  }
+
+  function touch(e) {
+    var target = e.target;
+    if (target.classList.contains("card")) {
+      target.classList.add("moving");
+
+      card = target;
+      mousedown = true;
+    }
+  }
+})();
