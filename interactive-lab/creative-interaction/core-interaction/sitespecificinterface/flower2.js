73ee7817869d95bca821fcf96e31ad9e10e3be1b
swjuri <31457339+swjuri@users.noreply.github.com>
2018-05-09 17:36:12 -0400

    f
    f

--- interactive-lab/creative-interaction/core-interaction/sitespecificinterface/flower2.js
+++ interactive-lab/creative-interaction/core-interaction/sitespecificinterface/flower2.js
@@ -11,6 +11,7 @@ document.getElementById("quick").onclick = function() {
 
 }
 
+
 document.getElementById("tries").onclick = function() {
 
     document.getElementById("numbertries").style.display = "inline-block";
@@ -23,6 +24,78 @@ document.getElementById("every").onclick = function() {
 
     document.getElementById("numberevery").style.display = "inline-block";
     document.getElementById("time").style.display = "none";
+
+
+}
+
+document.getElementById("quickk").onclick = function() {
+
+    document.getElementById("finalone").style.display = "inline-block";
+    document.getElementById("numberquick").style.display = "none";
+
+
+}
+
+document.getElementById("triess").onclick = function() {
+
+    document.getElementById("finaltwo").style.display = "inline-block";
+    document.getElementById("numberquick").style.display = "none";
+
+
+}
+
+document.getElementById("everyy").onclick = function() {
+
+    document.getElementById("finaltwo").style.display = "inline-block";
+    document.getElementById("numberquick").style.display = "none";
+
+
+}
+
+document.getElementById("quickkk").onclick = function() {
+
+    document.getElementById("finalone").style.display = "inline-block";
+    document.getElementById("numbertries").style.display = "none";
+
+
+}
+
+document.getElementById("triesss").onclick = function() {
+
+    document.getElementById("finalsix").style.display = "inline-block";
+    document.getElementById("numbertries").style.display = "none";
+
+
+}
+
+document.getElementById("everyyy").onclick = function() {
+
+    document.getElementById("finalfour").style.display = "inline-block";
+    document.getElementById("numbertries").style.display = "none";
+
+
+}
+
+document.getElementById("everyyyy").onclick = function() {
+
+    document.getElementById("finalfour").style.display = "inline-block";
+    document.getElementById("numberevery").style.display = "none";
+
+
+}
+
+document.getElementById("triessss").onclick = function() {
+
+    document.getElementById("finalfive").style.display = "inline-block";
+    document.getElementById("numberevery").style.display = "none";
+
+
+}
+
+document.getElementById("quickkkk").onclick = function() {
+
+    document.getElementById("finalsix").style.display = "inline-block";
+    document.getElementById("numberevery").style.display = "none";
 
 
 }
