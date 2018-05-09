d8a554c55f55a70d87492d4e7b7e424452f5bdb8
swjuri <31457339+swjuri@users.noreply.github.com>
2018-05-08 23:02:44 -0400

    n
    n

---
+++ interactive-lab/creative-interaction/core-interaction/sitespecificinterface/section.js
@@ -0,0 +1,21 @@
+document.getElementById("button-one").onclick = function() {
+    document.getElementById("photo").style.display = "inline-block";
+    document.getElementById("arrive").style.display = "inline-block";
+    document.getElementById("main").style.display = "none";
+    document.getElementById("instruct").style.display = "none";
+}
+
+document.getElementById("button-two").onclick = function() {
+    document.getElementById("photot").style.display = "inline-block";
+    document.getElementById("arrive2").style.display = "inline-block";
+    document.getElementById("main").style.display = "none";
+    document.getElementById("instruct").style.display = "none";
+}
+
+
+document.getElementById("button-three").onclick = function() {
+    document.getElementById("photor").style.display = "inline-block";
+    document.getElementById("arrive3").style.display = "inline-block";
+    document.getElementById("main").style.display = "none";
+    document.getElementById("instruct").style.display = "none";
+}
