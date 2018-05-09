0af9f4dce4bb8905ea138a2568a6d7ba6712426a
swjuri <31457339+swjuri@users.noreply.github.com>
2018-05-09 14:35:40 -0400

    m

---
+++ interactive-lab/creative-interaction/core-interaction/sitespecificinterface/arti.js
@@ -0,0 +1,31 @@
+document.getElementById("arti").onclick = function() {
+    document.getElementById("artii").style.display = "inline-block";
+    document.getElementById("arti").style.display = "none";
+
+}
+
+
+
+document.getElementById("on").onclick = function() {
+    document.getElementById("onon").style.display = "inline-block";
+    document.getElementById("artii").style.display = "none";
+
+}
+
+document.getElementById("deep").onclick = function() {
+    document.getElementById("deepdeep").style.display = "inline-block";
+    document.getElementById("artii").style.display = "none";
+
+}
+
+document.getElementById("deepdeep").onclick = function() {
+    document.getElementById("deepfruit").style.display = "inline-block";
+    document.getElementById("deepdepp").style.display = "none";
+
+}
+
+document.getElementById("onon").onclick = function() {
+    document.getElementById("onfruit").style.display = "inline-block";
+    document.getElementById("onon").style.display = "none";
+
+}
---
+++ interactive-lab/creative-interaction/core-interaction/sitespecificinterface/aspara.js
@@ -0,0 +1,31 @@
+document.getElementById("aspara").onclick = function() {
+    document.getElementById("asparaa").style.display = "inline-block";
+    document.getElementById("aspara").style.display = "none";
+
+}
+
+
+
+document.getElementById("yess").onclick = function() {
+    document.getElementById("asparayes").style.display = "inline-block";
+    document.getElementById("asparaa").style.display = "none";
+
+}
+
+document.getElementById("noo").onclick = function() {
+    document.getElementById("asparano").style.display = "inline-block";
+    document.getElementById("asparaa").style.display = "none";
+
+}
+
+document.getElementById("asparano").onclick = function() {
+    document.getElementById("nofruit").style.display = "inline-block";
+    document.getElementById("asparano").style.display = "none";
+
+}
+
+document.getElementById("asparayes").onclick = function() {
+    document.getElementById("yesfruit").style.display = "inline-block";
+    document.getElementById("asparayes").style.display = "none";
+
+}
