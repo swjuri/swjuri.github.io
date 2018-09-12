7690bb0c632a2d3972d49b6ffa7b9e336b1e721e
swjuri <31457339+swjuri@users.noreply.github.com>
2018-09-12 16:30:36 -0400

    d

---
+++ interactive-lab/creative-interaction/core-interaction/advance-java/homepage.js
@@ -0,0 +1,134 @@
+$("img").hide();
+
+$(".h1Btn").hover(
+	function() {
+		$(".h1")
+			.find("img")
+			.show();
+	},
+	function() {
+		$(".h1")
+			.find("img")
+			.hide();
+	}
+);
+
+$(".h1Btn").mousemove(function(e) {
+	$(".h1")
+		.css("left", e.pageX + 20)
+		.css("top", e.pageY + 20);
+});
+
+$(".h2Btn").hover(
+	function() {
+		$(".h2")
+			.find("img")
+			.show();
+	},
+	function() {
+		$(".h2")
+			.find("img")
+			.hide();
+	}
+);
+
+$(".h2Btn").mousemove(function(e) {
+	$(".h2")
+		.css("left", e.pageX + 20)
+		.css("top", e.pageY + 20);
+});
+
+$(".h3Btn").hover(
+	function() {
+		$(".h3")
+			.find("img")
+			.show();
+	},
+	function() {
+		$(".h3")
+			.find("img")
+			.hide();
+	}
+);
+
+$(".h3Btn").mousemove(function(e) {
+	$(".h3")
+		.css("left", e.pageX + 20)
+		.css("top", e.pageY + 20);
+});
+
+$(".h4Btn").hover(
+	function() {
+		$(".h4")
+			.find("img")
+			.show();
+	},
+	function() {
+		$(".h4")
+			.find("img")
+			.hide();
+	}
+);
+
+$(".h4Btn").mousemove(function(e) {
+	$(".h4")
+		.css("left", e.pageX + 20)
+		.css("top", e.pageY + 20);
+});
+
+$(".h4Btn").hover(
+	function() {
+		$(".h5")
+			.find("img")
+			.show();
+	},
+	function() {
+		$(".h5")
+			.find("img")
+			.hide();
+	}
+);
+
+$(".h4Btn").mousemove(function(e) {
+	$(".h5")
+		.css("left", e.pageX + 20)
+		.css("top", e.pageY + 20);
+});
+
+$(".h4Btn").hover(
+	function() {
+		$(".h6")
+			.find("img")
+			.show();
+	},
+	function() {
+		$(".h6")
+			.find("img")
+			.hide();
+	}
+);
+
+$(".h4Btn").mousemove(function(e) {
+	$(".h6")
+		.css("left", e.pageX + 20)
+		.css("top", e.pageY + 20);
+});
+
+$(".h4Btn").hover(
+	function() {
+		$(".h7")
+			.find("img")
+			.show();
+	},
+	function() {
+		$(".h7")
+			.find("img")
+			.hide();
+	}
+);
+
+$(".h4Btn").mousemove(function(e) {
+	$(".h7")
+		.css("left", e.pageX + 20)
+		.css("top", e.pageY + 20);
+});
